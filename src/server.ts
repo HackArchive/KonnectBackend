c8623af5d601603465454a8b49f5024e15a43d32
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-04-19 01:06:24 +0530

    Init node backend

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import * as dotenv from "dotenv";
+import cors = require("cors");
+import authRouter from './routes/auth.routes';
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument = require("../swagger.json");
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use("/api/auth", authRouter);
+
+
+app.listen(3001, ()=> {
+    console.log("application running")
+})
