ba110c1f7392d4b947d8f6a629dfe79222868a83
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-04-20 07:36:47 +0530

    dalle api added

--- src/common.ts
+++ src/common.ts
@@ -3,5 +3,4 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const AUTH_TOKEN = process.env.AUTH_TOKEN;
-export const ECOSYSTEM_ID = process.env.ECOSYSTEM_ID;
+export const DALLE_API = process.env.DALLE_API;
