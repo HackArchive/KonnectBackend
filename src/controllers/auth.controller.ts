cc281f284fee989cc156627728a481e6da43aeb4
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-04-21 11:32:37 +0530

    gsp bucket storage integrated

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,22 +1,26 @@
 import { IdentityProvider, TrinsicService } from '@trinsic/trinsic';
 import { Request, Response } from 'express';
-import { trinsic, ECOSYSTEM_ID } from '../common';
 import { prisma } from '../Prisma';
+import { openai, storage } from '../common';
 
-export const sendOTP = async (req: Request,res: Response) => {
 
-    try {
+export const generateImage = async (req: Request,res: Response) => {
 
-        let authInitResponse = await trinsic.wallet().authenticateInit({
-            identity: req.body.email,
-            provider: IdentityProvider.Email,
-            ecosystemId: ECOSYSTEM_ID
-        });
-        
+    try {   
+
+        let dalle_api_prompt = `Generate a realistic image of a model captured with a Nikon D850 and a Nikon AF-S NIKKOR 70-200mm f/2.8E FL ED VR lens, lit with high-key lighting to create a soft and ethereal feel, with a shallow depth of field --ar 2:3- with the following attributes: ${req.body.model_description}`
+        const data = await openai.images.generate({
+            model: "dall-e-3",
+            prompt:dalle_api_prompt,
+            size:"1024x1024",
+            quality:"hd",
+            n:1,
+        })
+
+       
         res.send({
             status: "success",
-            code: 0, 
-            ...authInitResponse
+            data: data.data[0], 
         });
             
 
@@ -30,3 +34,27 @@ export const sendOTP = async (req: Request,res: Response) => {
 
 }
 
+
+
+export const uploadProfilePic = async (req: Request,res: Response) => {
+
+    try {
+
+        console.log(req.file?.destination)
+        const gcs = storage.bucket("bucket_name"); // Removed "gs://" from the bucket name
+        const storagepath = `profile-pics/${req.file?.filename}`;
+        const result = await gcs.upload(`${req.file?.destination}/${req.file?.filename}`, {
+            destination: storagepath,
+            predefinedAcl: 'publicRead', // Set the file to be publicly readable
+            metadata: {
+                contentType: "application/plain", // Adjust the content type as needed
+            }
+        });
+        return result[0].metadata.mediaLink;
+        
+    } catch (error: any) {
+        console.log(error);
+        throw new Error(error.message);
+    }
+
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express"
-import {sendOTP } from "../controllers/auth.controller";
+import {generateImage,uploadProfilePic } from "../controllers/auth.controller";
+import multer from "multer";
+import path from "path";
 
 const router = Router();
 
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: (req, file, callback) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        callback(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
 
-router.post("/send-otp", sendOTP)
+router.post("/generate-image", generateImage)
+router.post("/upload-profile-pic", upload.single('image'),uploadProfilePic)
 
 
 export default router;
